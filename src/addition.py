c770a3118c5752fe69a166515540843773b75f7b
dosman001 <dosunmu.afeez1@gmail.com>
2024-11-07 10:49:17 +0100

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,8 @@
 # app.py
 # This is a test commit
-def add(a, b):
+def add(a, b);
     return a + b
 
 def test_add():
-    assert add(1, 2) == 9
+    assert add(1, 2) == 3
     assert add(1, -1) == 0
